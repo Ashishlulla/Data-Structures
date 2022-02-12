7188b77a3c642f3f53fc333bfb3ccf1afc0e9776
Ashish Rajesh Lulla <lullaashish2807@gmail.com>
2022-02-12 10:18:06 +0530

    Update directed_weighted.py

--- Graphs/Adjacency_matrix_representation/directed_weighted.py
+++ Graphs/Adjacency_matrix_representation/directed_weighted.py
@@ -25,3 +25,29 @@ def add_edges(v1, v2, cost):
         index1 = nodes.index(v1)
         index2 = nodes.index(v2)
         graph[index1][index2] = cost
+
+        
+def delete_node(v):
+    global node_count
+    if v not in nodes:
+        print(v,"Node is Not present")
+    else:
+        index1 = nodes.index(v)
+        node_count -= 1
+        nodes.remove(v)
+        graph.pop(index1)
+        for i in graph:
+            i.pop(index1)
+
+def delete_edge(v1, v2):
+    if v1 is not nodes:
+        print(v1, "Not present in Graph")
+    elif v2 is not nodes:
+        print(v2, "Not present in Graph")
+    else:
+        index1 = nodes.index(v1)
+        index2 = nodes.index(v2)
+        graph[index1][index2] = 0        
+        
+        
+        
