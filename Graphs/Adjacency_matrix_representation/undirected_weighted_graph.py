c59d0a447d8a5c7b427567e7b1e914c29582756a
Ashish Rajesh Lulla <lullaashish2807@gmail.com>
2022-02-12 10:20:21 +0530

    Update undirected_weighted_graph.py

--- Graphs/Adjacency_matrix_representation/undirected_weighted_graph.py
+++ Graphs/Adjacency_matrix_representation/undirected_weighted_graph.py
@@ -53,3 +53,9 @@ def delete_edge(v1, v2):
         index2 = nodes.index(v2)
         graph[index1][index2] = 0
         graph[index2][index1] = 0
+
+def print_grapgh():
+    for i in range(node_count):
+        for j in range(node_count):
+            print(format(graph[i][j], "<3"),end=" ")
+        print("\n")
