4e10099edba90c74be17498183290f95c87dd504
Ashish Rajesh Lulla <lullaashish2807@gmail.com>
2022-02-13 11:21:05 +0530

    Create directed_unweighted.py

---
+++ Graphs/Adjacency_list_representation/directed_unweighted.py
@@ -0,0 +1,26 @@
+graphdict = {}
+
+def add_node(v):
+    if v in graphdict:
+        print(v, "Node is Already present")
+    else:
+        graphdict.update({v:[]})
+
+def add_edge(v1,v2):
+    if v1 not in graphdict:
+        print(v1, "Node not present in Graph")
+    elif v2 not in graphdict:
+        print(v2, "Node not present in Graph")
+    else:
+        graphdict[v1].append(v2)
+
+def delete_node(v):
+    if v not in graphdict:
+        print(v,"Not present in Graph")
+    else:
+        graphdict.pop(v)
+        for i in graphdict.values():
+            if v in i:
+                i.remove(v)
+            else:
+                pass
